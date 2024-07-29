960a0b0be50b554374c51440cbf8a2cdb01dfae5
gabriellm-dev <gabriellm.dev@gmail.com>
2024-07-29 10:42:55 -0300

    first commit

---
+++ src/main/java/desafio_resfull_java/DesafioDioJavaApplication.java
@@ -0,0 +1,13 @@
+package desafio_resfull_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioJavaApplication.class, args);
+	}
+
+}
